3851025d8f025732f295ede0fe2b05cf8f41de73
Görkem Efe Dersin <dersingrkmefe2003@gmail.com>
2024-08-28 12:44:16 +0300

    Cart Angular
    Cart Angular

---
+++ cart/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  posts = [
+
+    {
+      title: 'Dağ Bisikleti',
+      imageUrl: '/assets/biking.jpeg',
+      username: 'cbdag',
+      content: 'Çok hızlı sürdüm'
+    },
+    {
+      title: 'Tırmanış',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'cbuludag',
+      content: 'Bugünde iyi tırmandım'
+    },
+    {
+      title: 'Doğa Yürüyüşü',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'cbdoga',
+      content: 'Hadi biraz Maçka Parkında turlayalım'
+    }
+
+  ];
+}
